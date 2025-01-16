7757cae9928255515cd52503b50da1cdfe52e55b
pasinsiri <pasin_plp0206@hotmail.com>
2025-01-16 20:30:24 +0700

    add scrape URL

--- functions/serper.py
+++ functions/serper.py
@@ -6,3 +6,4 @@ class Serper():
     def __init__(self, api_key):
         self.api_key = api_key
         self.search_url = "https://api.serpstack.com/search"
+        self.scrape_url = "https://scrape.serper.dev"
